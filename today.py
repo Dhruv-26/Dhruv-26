2f91c1e7ad91483b25a7c4517cf81ca45e5d2076
Dhruv_Kumar <73277299+Dhruv-26@users.noreply.github.com>
2025-02-05 18:46:52 +0530

    Update today.py

--- today.py
+++ today.py
@@ -449,7 +449,7 @@ if __name__ == '__main__':
     OWNER_ID, acc_date = user_data
     formatter('account data', user_time)
     age_data, age_time = perf_counter(daily_readme, datetime.datetime(1999, 4, 8))
-    formatter('age', age_data)
+    print('age', age_data)
     formatter('age calculation', age_time)
     total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
     formatter('LOC (cached)', loc_time) if total_loc[-1] else formatter('LOC (no cache)', loc_time)
